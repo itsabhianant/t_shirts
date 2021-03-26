5829696ca3be3eb90d2c0c4f6fcacef2e4f3b98c
itsabhianant <itsabhianant@gmail.com>
2021-03-26 07:52:17 +0530

    Well organised
    This is a well organized program with proper word wrappings and required comments making the code easy to read and understand.

--- tshirts.py
+++ tshirts.py
@@ -1,14 +1,18 @@
-# This is a program that takes input from you to take an order for customized t-shirts.
+# This is a program that takes input from you to take an order for 
+# customized t-shirts.
 
 def make_shirts():
     """This will make the shirt with a custom size and message."""
     listOfSizes = ['s', 'm', 'l', 'xl', 'xxl']
     active = True
 
+# Asking the size:
+
     while active:
 
         print("\n(enter 'q' to exit any time)")
-        size = input("What size of t-shirt do you want?(S/M/L/XL/XXL): ")
+        size = input("What size of t-shirt do you "\
+            "want?(S/M/L/XL/XXL): ")
 
         if size == 'q':
             print("\nThanks for visiting:)")
@@ -16,16 +20,24 @@ def make_shirts():
         if size.lower() not in listOfSizes:
             print("\nPlease choose correct size (S/M/L/XL/XXL).")
             continue
+        
+        # Asking the message user wants to get printed:
 
         print("\n(enter 'q' to exit any time)")
-        message = input("What do you want to be printed on your t-shirt: ")
+        message = input("What do you want to be printed on your"\
+            " t-shirt:")
         if message == 'q':
             print("\nThanks for visiting.:)")
             break
-
-        print("\nThis is to confirm your order that you want a tshirt of size: " + size.title().upper() + " and the message printed should be: " + message)
         
-        confirmation = input("\nPress y to confirm and n to rechoose your order. Press q to quit ")
+        # Confirmation:
+
+        print("\nThis is to confirm your order that you want a "\
+            "tshirt of size: " + size.title().upper() + " and the "\
+            "message printed should be: " + message)
+        
+        confirmation = input("\nPress y to confirm and n to rechoose "\
+            "your order. Press q to quit ")
         active_2 = True
 
         while active_2:
@@ -45,6 +57,7 @@ def make_shirts():
                 active_2 = False
                 
             else:
-                confirmation = input("\nChoose only 'y'/'n'/'q' for yes, no and quit respectively: ")
+                confirmation = input("\nChoose only 'y'/'n'/'q' for "\
+                     "yes, no and quit respectively: ")
                 continue
 make_shirts()
